5178b9a8d7926a4e573d2b97eb4734e862187625
Gracelyn Prajna <gracelynprajna@gmail.com>
2023-02-01 20:06:14 -0500

    made a test

--- server.test.js
+++ server.test.js
@@ -1,5 +1,18 @@
 import request from "supertest";
+import createServer from "./server.js";
+
+const server = await createServer();
 
 describe("Just testing the server", function(){
-    
-})
+    describe("testing the /todo route", function(){
+        it("Should be unable to get todos without flag", function(done){
+            request(server).get("/todo").expect(401).end(function(err){
+                if(err){
+                    throw err;
+                }else {
+                    done();
+                }
+            })
+        })
+    })
+});
